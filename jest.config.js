e0d0fb301ff9b9cf04d9b3abc9591f18eac82e59
risha-parveen <rishaparveen12@gmail.com>
2024-12-08 22:17:39 +0200

    Added configuration for ESM module

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
-module.exports = {
+const config = {
     collectCoverage: true,
     coverageDirectory: "coverage",
     coverageReporters: ["lcov", "text"],
+    transform: {
+        '^.+\\.[tj]s$': 'babel-jest', // Transform JavaScript and TypeScript files
+    },
 };
+
+export default config;
